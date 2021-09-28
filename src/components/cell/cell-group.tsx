c76c841372afc8ddc8b921a3479fa3a439048303
tangtanglove <dai_hang_love@126.com>
2021-09-28 18:00:35 +0800

    wip

---
+++ src/components/cell/cell-group.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { View } from '@tarojs/components'
+
+const CellGroup: React.FC<any> = (props:any) => {
+
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+export default CellGroup;
